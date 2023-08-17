fdf93554729653cbc31322213c15a1a3575252b5
Ali Qornan <ali.qornan@go-jek.com>
2023-08-17 12:17:48 +0700

    [Ali] Support multiple IDs

--- app/src/main/java/com/aliqornan/inv/match/parser/App.java
+++ app/src/main/java/com/aliqornan/inv/match/parser/App.java
@@ -9,6 +9,8 @@ import com.gojek.Figaro;
 import com.google.gson.Gson;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -25,15 +27,17 @@ public class App {
             PlayerStatReportGenerator playerStatReportGenerator = new PlayerStatReportGenerator(csvGenerator, configuration);
 
             /* SAMPLE: 7281893156 */
-            String matchId = args[0];
+            String[] matchIds = args[0].split(",");
+            List<PlayerStat> playerStats = new ArrayList<>();
+            for(String id : matchIds) {
+                System.out.println("Parsing match " + id);
+                List<PlayerStat> parsedStats = matchParser.parsePlayerStats(id);
+                System.out.println("Successfully parsed with result " + parsedStats);
+                playerStats.addAll(parsedStats);
+            }
 
-            System.out.println("Parsing match " + matchId);
-            List<PlayerStat> parsedStats = matchParser.parsePlayerStats(matchId);
-            System.out.println("Successfully parsed with result " + parsedStats);
-
-            System.out.println("Generating CSV");
-            File reportResult = playerStatReportGenerator.generate(parsedStats);
-            System.out.println("Successfully generated CSV with result " + reportResult.getAbsolutePath());
+            File result = playerStatReportGenerator.generate(playerStats);
+            System.out.println("Successfully generated CSV with result " + result.getAbsolutePath());
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException(e);
